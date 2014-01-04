bee538a86574ca1b03423a419d3631f9b4ed6ef6
Alberto De Bortoli <albertodebortoli@gmail.com>
2014-01-04 21:31:42 +0000

    Initial Commit

---
+++ bacetto/ADBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ADBAppDelegate.h
+//  bacetto
+//
+//  Created by Alberto De Bortoli on 04/01/2014.
+//  Copyright (c) 2014 Alberto De Bortoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ bacetto/ADBViewController.h
@@ -0,0 +1,13 @@
+//
+//  ADBViewController.h
+//  bacetto
+//
+//  Created by Alberto De Bortoli on 04/01/2014.
+//  Copyright (c) 2014 Alberto De Bortoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADBViewController : UIViewController
+
+@end
